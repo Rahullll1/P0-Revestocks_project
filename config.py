36d603b65de03a45c41cf611d0a865102158d771
Rahullll1 <vigneshchandra.p@gmail.com>
2025-04-22 16:28:44 +0530

    first

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',         # Replace with your MySQL username
+    'password': 'Rahool.111',     # Replace with your MySQL password
+    'database': 'revstock_db'        # Replace with your desired DB name
+}
